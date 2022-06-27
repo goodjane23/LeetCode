c6db414ba0c34afdab4e5f0a4143ffaaaf7ca4e4
Ekaterina Razimovich <apochemubinet@gmail.com>
2022-06-27 17:02:12 +0300

    Add description for task

--- MinNumberDeci-BinaryNum/Program.cs
+++ MinNumberDeci-BinaryNum/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+/// <summary>
+/// A decimal number is called deci-binary if each of its digits is
+/// either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary,
+/// while 112 and 3001 are not.
+/// Given a string n that represents a positive decimal integer, return the minimum number of 
+/// positive deci-binary numbers needed so that they sum up to n.
+/// </summary>
+
 
 namespace MinNumberDeci_BinaryNum
 {
