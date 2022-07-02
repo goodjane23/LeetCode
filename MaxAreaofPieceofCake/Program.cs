128c0b46cefc547013b98bf5a94d22f4d579036f
Ekaterina Razimovich <apochemubinet@gmail.com>
2022-07-02 22:09:33 +0300

    and one

---
+++ MaxAreaofPieceofCake/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+///<summary>
+///You are given a rectangular cake of size h x w and two arrays of integers horizontalCuts and verticalCuts where:
+///horizontalCuts[i] is the distance from the top of the rectangular cake to the ith horizontal cut and similarly, and
+///verticalCuts[j] is the distance from the left of the rectangular cake to the jth vertical cut.
+///Return the maximum area of a piece of cake after you cut at each horizontal and vertical position 
+///provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a large number, return this modulo 109 + 7.
+/// </summary>
+
+namespace MaxAreaofPieceofCake
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxArea(1000000000, 1000000000, new int[] { 2 }, new int[] { 2 });
+        }
+
+        public static int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+        {
+            
+            long maxHorizontal = FindMax(horizontalCuts, h);
+            long maxVertical = FindMax(verticalCuts, w);
+            return (int)((int) maxHorizontal*maxVertical % 1000000007);
+
+        }
+        public static int FindMax(int[] arr, int lngth)
+        {
+            Array.Resize(ref arr, arr.Length+2);
+            arr[arr.Length - 1] = lngth;
+            
+            Array.Sort(arr);
+            int max = 0;
+            for (int i = arr.Length-1; i > 0; i--)
+            {
+                var temp = arr[i] - arr[i - 1];
+                if (temp > max)
+                    max = temp;
+            }
+            return max;
+        }
+
+    }
+}
---
+++ MaximumUnitsTruck/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///<summary>
+///You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, 
+///where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+///     ()numberOfBoxesi is the number of boxes of type i.
+///     numberOfUnitsPerBoxi is the number of units in each box of the type i.
+/// You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. 
+/// You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+/// Return the maximum total number of units that can be put on the truck.
+///</summary>
+namespace MaximumUnitsTruck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] boxTypes = new int[3][];
+            boxTypes[0] = new int[2] { 1,3} ;
+            boxTypes[1] = new int[2] { 2,2};
+            boxTypes[2] = new int[2] {3,1};
+
+            Console.WriteLine(MaximumUnits( boxTypes, 4 ));
+        }
+        public static int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            int result = 0;
+            int max = 0;
+            int indexMax;
+            for (int i = 0; i < boxTypes.Length; i++)
+            {              
+                for (int j = 0; j < 2; j++)
+                {
+                    if (boxTypes[i][1] > max)
+                    {
+                        max = boxTypes[i][1];
+                        indexMax = boxTypes[i][j];
+                    }  
+
+                }
+            }
+            return 0;
+
+        }
+
+        public static void FindMax()
+        {
+            int max = 0;
+            
+        }
+    }
+}
+
