4751a160de5a439bd253c91b177de00125dab960
Ekaterina Razimovich <apochemubinet@gmail.com>
2022-09-21 22:18:58 +0300

    add files

---
+++ MaximumLengthofRepeatedSubarray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumLengthofRepeatedSubarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FindLength(int[] nums1, int[] nums2)
+        {
+            int maxLength;
+
+            if (nums1.Length > nums2.Length)
+            {
+                maxLength = nums1.Length;
+            }
+            else maxLength = nums1.Length;
+
+            return 0;
+        }
+    }
+}
---
+++ SumEvenNumbersAfterQueries/Program.cs
@@ -0,0 +1,37 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public int[] SumEvenAfterQueries(int[] nums, int[][] queries)
+    {
+        int[] answer = new int[nums.Length];
+        int evenSum = nums.Where(x => x % 2 == 0).Sum();
+        int index = 0;
+
+        foreach (var query in queries)
+        {
+            var originalNum = nums[query[1]];
+            var changedNum = nums[query[1]] += query[0];
+
+            if (changedNum % 2 == 0)
+            {
+                if (originalNum % 2 == 0)
+                    evenSum += query[0];
+                else
+                    evenSum += changedNum;
+            }
+            else
+            {
+                if (originalNum % 2 == 0)
+                    evenSum -= originalNum;
+            }
+            answer[index++] = evenSum;
+        }
+        return answer;
+    }
+    
+}
