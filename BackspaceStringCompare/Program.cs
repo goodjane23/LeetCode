184914e3a3141f0d5f9b68303b4a37819c18fadd
goodjane23 <apochemubinet@gmail.com>
2023-10-19 21:48:20 +0300

    844. Backspace String Compare solve

---
+++ BackspaceStringCompare/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+/// <summary>
+/// Given two strings s and t, return true if they are equal when both are typed into empty 
+/// text editors. '#' means a backspace character.
+/// Note that after backspacing an empty text, the text will continue empty.
+/// </summary>
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"{BackspaceCompare("c#d#", "ab##")}");
+        //true:
+        Console.WriteLine($"{BackspaceCompare("ab#c", "ad#c")}");        
+        Console.WriteLine($"{BackspaceCompare("a##c", "#a#c")}");
+        //false:
+        Console.WriteLine($"{BackspaceCompare("a#c", "b")}");        
+        Console.WriteLine($"{BackspaceCompare("uio##cc", "uio###cc")}");
+    }
+
+    public static bool BackspaceCompare(string s, string t)
+    {       
+        return Transform(s).Equals(Transform(t));
+    }
+
+    private static string Transform(string s) 
+    { 
+        var result = string.Empty;
+
+        for (int i = 0; i < s.Length-1; i++)
+        {
+            if (s[i] is '#')
+            {
+                if (result.Length>0)
+                {
+                    result = result.Remove(result.Length-1, 1);
+                }
+            }
+            else
+            {
+                result+= s[i];
+            }
+        }
+        if (s.Last() == '#' && result.Length>0)
+        {
+            result = result.Remove(result.Length-1,1);
+        }
+        else
+        {
+            result += s.Last();
+        }
+        return result;
+    }
+}
---
+++ RotateArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+/// <summary>
+/// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        Rotate(nums, 3);        
+    }
+    private static void Rotate(int[] nums, int k)
+    {
+        var list = nums.ToList();
+        var kn = list.TakeLast(k);
+        list.AddRange(kn);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = list[i];
+        }
+    }
+}
