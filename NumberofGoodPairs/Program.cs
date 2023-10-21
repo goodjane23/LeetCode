957d18c77acd2e3c6f994dac71a6b1218846c5d5
goodjane23 <apochemubinet@gmail.com>
2023-10-21 12:50:54 +0300

    1512. Number of Good Pairs

---
+++ NumberofGoodPairs/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+/// <summary>
+/// Given an array of integers nums, return the number of good pairs.
+/// A pair(i, j) is called good if nums[i] == nums[j] and i<j
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var res = NumIdenticalPairs(new int[]{ 1, 1, 1, 1 });
+        Console.WriteLine($"Count is: {res}");
+    }
+
+    public static int NumIdenticalPairs(int[] nums)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {           
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j]&& i<j)
+                {
+                    result++;
+                }
+            }    
+
+        }
+        return result;
+    }
+}
