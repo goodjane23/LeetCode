71215bffd1d52ecb290484d7e45d1e6624054736
GoodGateway <apochemubinet@gmail.com>
2024-01-22 00:07:28 +0300

    add solution  for 198 problem

---
+++ HouseRobber/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] nums = { 2, 1, 1, 2 };
+        Console.WriteLine(Rob(nums));
+    }
+
+    public static int Rob(int[] nums)
+    {
+        int prevMax = 0;
+        int currMax = 0;
+
+        foreach (int num in nums)
+        {
+            int temp = currMax;
+            currMax = Math.Max(prevMax + num, currMax);
+            prevMax = temp;
+        }
+
+        return currMax;
+    }
+}
