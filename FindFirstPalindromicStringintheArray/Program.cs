c24c78f51fe75fce18fd855c30bda002510e184b
GoodGateway <apochemubinet@gmail.com>
2024-02-13 19:52:06 +0300

    solution for 2108 problem

---
+++ FindFirstPalindromicStringintheArray/Program.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Given an array of strings words, return the first palindromic string in the array. 
+/// If there is no such string, return an empty string "".A string is palindromic if
+/// it reads the same forward and backward.
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(FirstPalindrome(["abc", "car", "ada", "racecar", "cool"]));
+        Console.WriteLine(FirstPalindrome(["notapalindrome", "racecar"]));
+        Console.WriteLine(FirstPalindrome(["def", "ghi"]));
+    }
+
+    public static string FirstPalindrome(string[] words)
+    {
+        foreach (string word in words)
+        {
+            if(IsPalindrome(word))
+                return word;
+        }
+
+        return string.Empty;       
+    }
+
+    private static bool IsPalindrome(string s)
+    {
+        var temp = "";
+        for (int i = s.Length-1; i >=0; i--)
+        {
+            temp += s[i];
+        }
+        return s.Equals(temp);
+    }
+}
