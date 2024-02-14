102c1d84c7c9605ecec211e436737cc950a97699
GoodGateway <apochemubinet@gmail.com>
2024-02-14 19:34:45 +0300

    add solution 74. Search a 2D Matrix

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //[2,4,3], l2 = [5,6,4]
+        ListNode listNode1 = new (2, new (4, new(3)));
+        ListNode listNode2 = new (5, new (6, new(4)));
+        AddTwoNumbers(listNode2, listNode1);
+        Console.ReadLine();
+    }
+
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = new ListNode();
+        var x1 = GetList(l1);
+        var x2 = GetList(l2);
+
+        var y1 = GetInt(x1);
+        var y2 = GetInt(x2);
+
+        var sum = (y1 + y2).ToString();
+
+        //Create listnode
+        for (int i = sum.Length-1; i >= 0; i--)
+        {
+            
+        }
+
+        return result;
+    }
+
+    private static List<int> GetList(ListNode l1)
+    {
+        List<int> ints = [l1.val];
+        var temp = l1.next;
+        while (temp is not null)
+        {
+            ints.Add(temp.val);
+            temp = temp.next;
+        }
+        return ints;
+    }
+
+    private static int GetInt(List<int> ints)
+    {
+        int n1 = 0;
+        for (int i = 0; i < ints.Count; i++)
+        {
+            var ten = (int)Math.Pow(10, i);
+            var num1 = ints[i] * ten;
+            n1 += num1;
+        }
+        return n1;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+ }
--- Search2DMatrix/Program.cs
+++ Search2DMatrix/Program.cs
@@ -1,33 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
+//You are given an m x n integer matrix matrix with the following two properties:
+//Each row is sorted in non-decreasing order.
+//The first integer of each row is greater than the last integer of the previous row.
+//Given an integer target, return true if target is in matrix or false otherwise.
+//You must write a solution in O(log(m * n)) time complexity.
 
 namespace Search2DMatrix
 {
-   
     internal class Program
     {
         static void Main(string[] args)
         {
             int[][] matrix = new int[5][];
-            matrix[0] = new int[] { 1, 4, 7, 11, 15 };
-            matrix[1] = new int[] { 2, 5, 8, 12, 19 };
-            matrix[2] = new int[] { 3, 6, 9, 16, 22 };
-            matrix[3] = new int[] { 10, 13, 14, 17, 24 };
-            matrix[4] = new int[] { 18, 21, 23, 26, 30 };
+            matrix[0] = [1, 4, 7, 11, 15];
+            matrix[1] = [2, 5, 8, 12, 19];
+            matrix[2] = [3, 6, 9, 16, 22];
+            matrix[3] = [10, 13, 14, 17, 24];
+            matrix[4] = [18, 21, 23, 26, 30];
             SearchMatrix(matrix, 5);
         }
 
         public static bool SearchMatrix(int[][] matrix, int target)
         {
-  
-
-            var x = Array.BinarySearch(matrix, target);
-            if (x>=0)
+            for (int i = 0; i < matrix.Length; i++)
             {
-                return true;
+                if (matrix[i].Contains(target)) return true;
+            }
+            return false;
+        }
+        //or
+        public bool SearchMatrix2(int[][] matrix, int target)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == target) return true;
+                }
             }
             return false;
         }
